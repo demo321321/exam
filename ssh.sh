4c963e2e1dbf9ec3fae56d19153e2d441f869932
demo321321 <marshedgehog24@gmail.com>
2026-05-05 19:57:12 +0500

    Create ssh.sh

---
+++ ssh.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Настройка SSH
+echo "Port 2026" >> /etc/openssh/sshd_config
+echo "AllowUsers sshuser" >> /etc/openssh/sshd_config
+echo "PermitRootLogin no" >> /etc/openssh/sshd_config
+echo "MaxAuthTries 2" >> /etc/openssh/sshd_config
+echo "Banner /root/banner" >> /etc/openssh/sshd_config
+
+# Создание баннера
+echo "banner    [-M---]" > /root/banner
+echo "Authorized access only" >> /root/banner
+echo "" >> /root/banner
+
+# Перезапуск и добавление в автозагрузку
+systemctl restart sshd
+systemctl enable sshd
