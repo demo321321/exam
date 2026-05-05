fbca2a10ded74fe59bf92db53d532bed2c7b3812
demo321321 <marshedgehog24@gmail.com>
2026-05-05 18:46:28 +0500

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-useradd sshuser -u 2026
-passwd sshuser
-P@ssw0rd
-P@ssw0rd
+useradd sshuser -u 2026 -m -s /bin/bash
+echo "sshuser:P@ssw0rd" | chpasswd
 usermod -aG wheel sshuser
 echo "Пользователь sshuser успешно создан"
