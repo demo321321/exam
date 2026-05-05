8b7862c9377e1f8fb281dfe381702de6947b7df0
demo321321 <marshedgehog24@gmail.com>
2026-05-05 20:49:47 +0500

    Create zad71.sh

---
+++ zad7.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Добавление репозитория Debian Buster
+echo "deb [trusted=yes] https://archive.debian.org/debian buster main" >> /etc/apt/sources.list
+
+# Обновление и установка ftr (frr)
+apt update
+apt install frr -y
+
+# Включение ospfd в демонах
+sed -i 's/ospfd=no/ospfd=yes/' /etc/frr/daemons
+
+# Включение и перезапуск frr
+systemctl enable frr
+systemctl restart frr
+
+# Настройка OSPF через vtysh
+vtysh << EOF
+conf t
+router ospf
+network 10.0.0.0/30 area 0
+network 192.168.3.0/28 area 0
+exit
+interface gre1
+ip ospf authentication message-digest
+ip ospf message-digest-key 1 md5 P@ssw0rd
+exit
+do wr mem
+exit
+EOF
+
+echo "Готово! OSPF настроен."
