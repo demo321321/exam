a8a90aa9be8938df1fa987952d5309c1a695a34d
demo321321 <marshedgehog24@gmail.com>
2026-05-06 16:00:35 +0500

    Update mod1.sh

--- mod1.sh
+++ mod1.sh
@@ -3,32 +3,43 @@
 # Копирование krb5.conf (без запроса подтверждения)
 \cp -f /var/lib/samba/private/krb5.conf /etc/krb5.conf 2>/dev/null
 
-# Включение Samba
+# Полная замена smb.conf
+cat > /etc/samba/smb.conf << 'EOF'
+# Global parameters
+[global]
+    dns forwarder = 192.168.1.2
+    netbios name = BR-SRU
+    realm = AU-TEAM.IRPO
+    server role = active directory domain controller
+    workgroup = AU-TEAM
+    interfaces = lo ens18
+    bind interfaces only = yes
+
+[sysvol]
+    path = /var/lib/samba/sysvol
+    read only = No
+
+[netlogon]
+    path = /var/lib/samba/sysvol/au-team.irpo/scripts
+    read only = No
+EOF
+
+# Включение и перезапуск Samba
 systemctl enable samba
-
-# Проверка, есть ли уже строки interfaces в smb.conf
-if ! grep -q "interfaces = lo ens18" /etc/samba/smb.conf; then
-    # Добавление настроек после секции [global]
-    sed -i '/\[global\]/a\
-interfaces = lo ens18\
-bind interfaces only = yes' /etc/samba/smb.conf
-    echo "Настройки interfaces добавлены в smb.conf"
-else
-    echo "Настройки interfaces уже присутствуют"
-fi
-
-# Перезапуск Samba
 systemctl restart samba
 
-samba-tool domain info 127.0.0.1
-
+# Создание пользователей
 samba-tool user create hquser1 P@ssw0rd
 samba-tool user create hquser2 P@ssw0rd
 samba-tool user create hquser3 P@ssw0rd
 samba-tool user create hquser4 P@ssw0rd
 samba-tool user create hquser5 P@ssw0rd
 
-samba-tool user list
-
+# Создание группы и добавление пользователей
 samba-tool group add hq
 samba-tool group addmembers hq hquser1,hquser2,hquser3,hquser4,hquser5
+
+# Проверка
+samba-tool user list
+
+echo "Готово! Samba DC настроен, пользователи и группа созданы"
