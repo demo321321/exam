4981a78b92803228dc6f6233c7e75fd3701d4b66
demo321321 <marshedgehog24@gmail.com>
2026-05-08 16:52:25 +0500

    Update mod1.sh

--- mod1.sh
+++ mod1.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
+samba-tool user create hquser1 P@ssw0rd
+samba-tool user create hquser2 P@ssw0rd
+samba-tool user create hquser3 P@ssw0rd
+samba-tool user create hquser4 P@ssw0rd
+samba-tool user create hquser5 P@ssw0rd
+
+samba-tool group add hq
+
+samba-tool group addmembers hq hquser1,hquser2,hquser3,hquser4,hquser5
 
