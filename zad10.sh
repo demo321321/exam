66cd3279d917dbcc4df9b768685d3dd909cfcd94
demo321321 <marshedgehog24@gmail.com>
2026-05-08 12:26:03 +0500

    Update zad10.sh

--- zad10.sh
+++ zad10.sh
@@ -14,13 +14,13 @@ listen-address=192.168.1.2
 no-resolv
 no-hosts
 
-address=/hg-rtr.au-team.irpo/192.168.1.1
-ptr-record=1.1.168.192.in-addr.arpa,hg-rtr.au-team.irpo
+address=/hq-rtr.au-team.irpo/192.168.1.1
+ptr-record=1.1.168.192.in-addr.arpa,hq-rtr.au-team.irpo
 address=/br-rtr.au-team.irpo/192.168.3.1
-address=/hg-srv.au-team.irpo/192.168.1.2
-ptr-record=2.1.168.192.in-addr.arpa,hg-srv.au-team.irpo
-address=/hg-cli.au-team.irpo/192.168.2.2
-ptr-record=2.2.168.192.in-addr.arpa,hg-cli.au-team.irpo
+address=/hq-srv.au-team.irpo/192.168.1.2
+ptr-record=2.1.168.192.in-addr.arpa,hq-srv.au-team.irpo
+address=/hq-cli.au-team.irpo/192.168.2.2
+ptr-record=2.2.168.192.in-addr.arpa,hq-cli.au-team.irpo
 address=/br-srv.au-team.irpo/192.168.3.2
 address=/docker.au-team.irpo/172.16.1.1
 address=/web.au-team.irpo/172.16.2.1
