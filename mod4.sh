9c4db1b5d44b58e07ff5303392f500455410076a
demo321321 <marshedgehog24@gmail.com>
2026-05-08 17:34:09 +0500

    Create mod4.sh

---
+++ mod4.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Установка chrony (автоматический ответ Yes)
+apt-get update && apt-get install -y chrony
+
+# Комментирование старой строки pool и добавление новых настроек
+sed -i 's/^pool 2.debian.pool.ntp.org iburst/#pool 2.debian.pool.ntp.org iburst/' /etc/chrony/chrony.conf
+
+# Добавление новых настроек в конец файла
+cat >> /etc/chrony/chrony.conf << 'EOF'
+
+# Добавленные настройки NTP
+server 0.ru.pool.ntp.org iburst prefer minstratum 4
+local stratum 5
+allow 0.0.0.0/0
+EOF
+
+# Перезапуск службы
+systemctl restart chronyd
+
+# Включение в автозагрузку
+systemctl enable chronyd
+
+echo "mod4b.sh"
