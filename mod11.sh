e6c3ed6019013b17daf33bc3247134f3f95c0c4a
demo321321 <marshedgehog24@gmail.com>
2026-05-11 21:19:20 +0500

    Update mod11.sh

--- mod11.sh
+++ mod11.sh
@@ -13,15 +13,4 @@ bind interfaces only = yes |
 # Перезапуск
 systemctl restart samba
 
-
-samba-tool user create hquser1 P@ssw0rd
-samba-tool user create hquser2 P@ssw0rd
-samba-tool user create hquser3 P@ssw0rd
-samba-tool user create hquser4 P@ssw0rd
-samba-tool user create hquser5 P@ssw0rd
-
-samba-tool group add hq
-
-samba-tool group addmembers hq hquser1,hquser2,hquser3,hquser4,hquser5
-
-echo "/etc/sudoers | %hq    ALL = NOPASSWD: /usr/bin/cat, /bin/grep, /usr/bin/id"
+echo "hq-cli"
