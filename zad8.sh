78a4c6a170a96b60fdb5e54858062f5dda90ed95
demo321321 <marshedgehog24@gmail.com>
2026-05-11 17:11:10 +0500

    Update zad8.sh

--- zad8.sh
+++ zad8.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+echo "iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT"
 iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+echo "iptables -A FORWARD -m conntrack --ctstate INVALID -j DROP"
 iptables -A FORWARD -m conntrack --ctstate INVALID -j DROP
 
 iptables-save > /etc/rules.v4
