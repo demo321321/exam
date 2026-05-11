78f6982f9e05c302598e83bed2b2d4e3d65acf8a
demo321321 <marshedgehog24@gmail.com>
2026-05-12 00:39:12 +0500

    Update mod5.sh

--- mod5.sh
+++ mod5.sh
@@ -8,7 +8,7 @@ cat > /etc/ansible/hosts << 'EOF'
 [servers]
 HQ-SRV ansible_host=192.168.1.2 ansible_user=sshuser ansible_password=P@ssw0rd ansible_port=2026
 HQ-CLI ansible_host=192.168.2.2 ansible_user=user ansible_password=resu
-HQ-RTR ansible_host=192.168.1.2 ansible_user=net_admin ansible_password=P@ssw0rd
+HQ-RTR ansible_host=192.168.1.1 ansible_user=net_admin ansible_password=P@ssw0rd
 BR-RTR ansible_host=192.168.3.1 ansible_user=net_admin ansible_password=P@ssw0rd
 
 [all:vars]
