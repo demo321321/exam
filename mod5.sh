16e5a48a16c80f716f00908a537221edac57bfb3
demo321321 <marshedgehog24@gmail.com>
2026-05-11 23:29:12 +0500

    Update mod5.sh

--- mod5.sh
+++ mod5.sh
@@ -15,16 +15,6 @@ BR-RTR ansible_host=192.168.3.1 ansible_user=net_admin ansible_password=P@ssw0rd
 ansible_python_interpreter=/usr/bin/python3
 EOF
 
-# Настройка ansible.cfg
-sed -i 's/#inventory      = /etc/ansible/hosts/inventory = /etc/ansible/hosts/' /etc/ansible/ansible.cfg 2>/dev/null
-if ! grep -q "inventory = /etc/ansible/hosts" /etc/ansible/ansible.cfg; then
-    echo -e "\n[defaults]\ninventory = /etc/ansible/hosts\nhost_key_checking = False" >> /etc/ansible/ansible.cfg
-fi
-
-# Отключение проверки ключей хоста
-if ! grep -q "host_key_checking = False" /etc/ansible/ansible.cfg; then
-    sed -i 's/#host_key_checking = False/host_key_checking = False/' /etc/ansible/ansible.cfg
-fi
 
 # Проверка
 ansible all -m ping
