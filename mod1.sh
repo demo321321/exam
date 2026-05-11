8ce062c453b2d7c4d9affc1ffb14569deac1248e
demo321321 <marshedgehog24@gmail.com>
2026-05-11 20:11:26 +0500

    Update mod1.sh

--- mod1.sh
+++ mod1.sh
@@ -9,23 +9,4 @@ apt-get install -y task-samba-dc
 # Удаление стандартного конфига
 rm -rf /etc/samba/smb.conf
 
-echo "samba-tool domain provision"
-
-# Копирование krb5.conf
-cp /var/lib/samba/private/krb5.conf /etc/krb5.conf
-
-# Отключение BIND
-systemctl disable bind --now 2>/dev/null
-
-# Включение Samba
-systemctl enable samba --now
-
-# Добавление настроек interfaces
-sed -i '/\[global\]/a\
-  interfaces = lo ens18\
-  bind interfaces only = yes' /etc/samba/smb.conf
-
-# Перезапуск
-systemctl restart samba
-
-echo "Готово!"
+echo "samba-tool domain provision | mod11.sh"
