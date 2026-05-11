66ae176a3e342320154b27961f01ee13724d1ec5
demo321321 <marshedgehog24@gmail.com>
2026-05-11 20:20:44 +0500

    Create mod11.sh

---
+++ mod11.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cp /var/lib/samba/private/krb5.conf /etc/krb5.conf
+
+# Отключение BIND
+systemctl disable bind --now 2>/dev/null
+
+# Включение Samba
+systemctl enable samba --now
+
+# Добавление настроек interfaces
+echo 'interfaces = lo ens18 |
+bind interfaces only = yes'
+
+# Перезапуск
+systemctl restart samba
