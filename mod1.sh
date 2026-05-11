8ef65859f8ff8b4964ea19c290a1ce5a686db307
demo321321 <marshedgehog24@gmail.com>
2026-05-11 20:08:45 +0500

    Update mod1.sh

--- mod1.sh
+++ mod1.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
-samba-tool user create hquser1 P@ssw0rd
-samba-tool user create hquser2 P@ssw0rd
-samba-tool user create hquser3 P@ssw0rd
-samba-tool user create hquser4 P@ssw0rd
-samba-tool user create hquser5 P@ssw0rd
+# Обновление пакетов
+apt-get update
 
-samba-tool group add hq
+# Установка Samba DC
+apt-get install -y task-samba-dc
 
-samba-tool group addmembers hq hquser1,hquser2,hquser3,hquser4,hquser5
+# Удаление стандартного конфига
+rm -rf /etc/samba/smb.conf
 
+echo "samba-tool domain provision"
+
+# Копирование krb5.conf
+cp /var/lib/samba/private/krb5.conf /etc/krb5.conf
+
+# Отключение BIND
+systemctl disable bind --now 2>/dev/null
+
+# Включение Samba
+systemctl enable samba --now
+
+# Добавление настроек interfaces
+sed -i '/\[global\]/a\
+interfaces = lo ens18\
+bind interfaces only = yes' /etc/samba/smb.conf
+
+# Перезапуск
+systemctl restart samba
+
+echo "Готово!"
