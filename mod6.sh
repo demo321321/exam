efca4ce723ce004e799215f92183dac88c71c9ee
demo321321 <marshedgehog24@gmail.com>
2026-05-12 02:03:22 +0500

    Update mod6.sh

--- mod6.sh
+++ mod6.sh
@@ -30,45 +30,44 @@ cd /root/testapp
 # Создание файла docker-compose.yaml
 cat > docker-compose.yaml << 'EOF'
 services:
- testapp:
-  image: site:latest
-  container_name: testapp
-  restart: always
-  depends_on:
-   - db
-  ports:
-   - 8080:8080
-  environment:
+1testapp:
+2image: site:latest
+2container_name: testapp
+2restart: always
+2depends_on:
+3- db
+2ports:
+3- 8080:8080
+2environment:
 
-   DB_TYPE: postgres
-   DB_HOST: db
-   DB_NAME: testdb
-   DB_PORT: 5432
-   DB_USER: test
-   DB_PASS: P@ssw0rd
+3DB_TYPE: postgres
+3DB_HOST: db
+3DB_NAME: testdb
+3DB_PORT: 5432
+3DB_USER: test
+3DB_PASS: P@ssw0rd
 
- db:
-  image: postgres:15-alpine
-  container_name: db
-  restart: always
-  environment:
-   POSTGRES_DB: testdb
-   POSTGRES_USER: test
-   POSTGRES_PASSWORD: P@ssw0rd
-  volumes:
-   - db_data:/var/lib/postgresql/data
+1db:
+2image: postgres:15-alpine
+2container_name: db
+2restart: always
+2environment:
+3POSTGRES_DB: testdb
+3POSTGRES_USER: test
+3POSTGRES_PASSWORD: P@ssw0rd
+2volumes:
+3- db_data:/var/lib/postgresql/data
 
 volumes:
- db_data:
+1db_data:
 EOF
 
-# Запуск контейнеров
-docker compose up -d
-
-# Проверка работающих контейнеров
-docker ps
 
 # Добавление задачи в crontab для автозапуска после перезагрузки
 (crontab -l 2>/dev/null; echo "@reboot cd /root/testapp && /usr/bin/docker compose up -d") | crontab -
 
-echo "cli http://192.168.3.2:8080"
+echo "cli http://192.168.3.2:8080
+
+docker compose up -d
+
+docker ps"
