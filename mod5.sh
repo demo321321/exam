0eed04670c955b090e777e50be38a860065782b0
demo321321 <marshedgehog24@gmail.com>
2026-05-12 17:01:24 +0500

    Update mod5.sh

--- mod5.sh
+++ mod5.sh
@@ -6,7 +6,7 @@ apt-get update && apt-get install -y ansible sshpass
 # Создание файла инвентаря /etc/ansible/hosts
 cat > /etc/ansible/hosts << 'EOF'
 HQ-SRV ansible_host=192.168.1.2 ansible_user=sshuser ansible_password=P@ssw0rd ansible_port=2026
-HQ-CLI ansible_host=192.168.2.2 ansible_user=user ansible_password=P@ssw0rd
+HQ-CLI ansible_host=192.168.2.3 ansible_user=user ansible_password=P@ssw0rd
 HQ-RTR ansible_host=192.168.1.1 ansible_user=net_admin ansible_password=P@ssw0rd
 BR-RTR ansible_host=192.168.3.1 ansible_user=net_admin ansible_password=P@ssw0rd
 
