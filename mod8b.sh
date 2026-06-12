8d55554f631e4313bb140b8774cbf9b00b67fb2c
demo321321 <marshedgehog24@gmail.com>
2026-06-12 20:39:38 +0500

    Update mod8b.sh

--- mod8b.sh
+++ mod8b.sh
@@ -5,6 +5,6 @@ iptables -t nat -A PREROUTING -d 172.16.1.2 -p tcp --dport 8080 -j DNAT --to-des
 iptables -t nat -A PREROUTING -d 172.16.1.2 -p tcp --dport 2026 -j DNAT --to-destination 192.168.1.2:2026
 
 # Сохранение конфигурации
-iptables-save > /etc/rules.v4
+iptables-save > /etc/iptables.rules
 
 echo "."
