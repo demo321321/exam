a9f4fde53c59f82b88eebba78ad5024a85f2ee6f
demo321321 <marshedgehog24@gmail.com>
2026-06-19 19:21:56 +0500

    Update mod21.sh

--- mod21.sh
+++ mod21.sh
@@ -1,1 +1,23 @@
 
+
+
+# Создание systemd-unit для автоматического монтирования
+cat > /etc/systemd/system/raid.mount << 'EOF'
+[Unit]
+Description=Mount RAID0
+
+[Mount]
+What=/dev/md0p1
+Where=/raid
+Type=ext4
+Options=defaults
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Включение и запуск юнита
+systemctl enable raid.mount --now
+
+# Перемонтирование всех файловых систем
+mount -a
