d2b34521bfe2e889f92036a14a83d6754acd2d5a
demo321321 <marshedgehog24@gmail.com>
2026-06-19 18:28:13 +0500

    Update zad9.sh

--- zad9.sh
+++ zad9.sh
@@ -7,32 +7,5 @@ apt-get install isc-dhcp-server -y
 # Настройка интерфейса в /etc/default/isc-dhcp-server
 sed -i 's/^INTERFACESv4=.*/INTERFACESv4="eth1.200"/' /etc/default/isc-dhcp-server
 
-# Создание резервной копии конфига
-cp /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.bkp
 
-# Комментирование строк option domain-name и option domain-name-servers
-sed -i 's/^option domain-name "/#option domain-name "/' /etc/dhcp/dhcpd.conf
-sed -i 's/^option domain-name-servers /#option domain-name-servers /' /etc/dhcp/dhcpd.conf
-
-# Раскомментирование строки authoritative
-sed -i 's/^#authoritative;/authoritative;/' /etc/dhcp/dhcpd.conf
-
-# Добавление конфигурации подсети в конец файла
-cat >> /etc/dhcp/dhcpd.conf << 'EOF'
-
-subnet 192.168.2.0 netmask 255.255.255.240 {
-    range 192.168.2.2 192.168.2.10;
-    option domain-name-servers 192.168.1.2;
-    option domain-search "au-team.irpo";
-    option routers 192.168.2.1;
-}
-EOF
-
-sleep 5
-# Перезапуск DHCP-сервера
-systemctl restart isc-dhcp-server
-
-# Добавление в автозагрузку
-systemctl enable isc-dhcp-server
-
-echo "скрин /etc/dhcp/dhcpd.conf"
+echo "zad91.sh"
