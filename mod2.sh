f2df0880033c56e6f3d5202f49be28d063c5eee7
demo321321 <marshedgehog24@gmail.com>
2026-06-19 19:25:56 +0500

    Update mod2.sh

--- mod2.sh
+++ mod2.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
+mkdir -p /raid
 # Создание RAID0 из дисков sdb и sdc
-mdadm --create /dev/md0 --level=0 --raid-devices=2 /dev/sdb /dev/sdc
+echo "mdadm --create /dev/md0 --level=[0] --raid-devices=[2] /dev/sdb /dev/sdc []"
 
 # Сохранение конфигурации RAID
-mdadm --detail --scan > /etc/mdadm.conf
+echo "mdadm --detail --scan > /etc/mdadm.conf"
 
 # Создание раздела на RAID-массиве через fdisk (автоматически)
-echo -e "n\np\n1\n\n\nw" | fdisk /dev/md0
+echo "echo -e ''n\np\n1\n\n\nw'' | fdisk /dev/md[0]"
 
 # Форматирование раздела в ext4
-mkfs.ext4 /dev/md0p1
-
-# Создание точки монтирования
-mkdir -p /raid
+echo "mkfs.ext4 /dev/md[0]p1"
 
 # Монтирование вручную
-mount /dev/md0p1 /raid
+echo "mount /dev/md[0]p1 /raid"
 
 # Создание systemd-unit для автоматического монтирования
 cat > /etc/systemd/system/raid.mount << 'EOF'
@@ -32,9 +30,3 @@ Options=defaults
 [Install]
 WantedBy=multi-user.target
 EOF
-
-# Включение и запуск юнита
-systemctl enable raid.mount --now
-
-# Перемонтирование всех файловых систем
-mount -a
